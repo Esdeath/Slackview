4bc290ff9e5eb5b814087de120ab00ce998dfdee
wangli <wanglii@foxmail.com>
2018-02-26 19:14:31 +0800

    add baidu tongji

--- footer.php
+++ footer.php
@@ -3,5 +3,15 @@
 
 </div>
 
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?516d9da2351546d5ead74b9d26a1fe86";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
+
 </body>
 </html>
